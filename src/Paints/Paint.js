e8c9495e68e65b3894378d6393468e523c7b564a
M Kumaran <veerammalkumaran@gmail.com>
2020-12-01 18:24:41 +0530

    sort issue on paint component resolved

--- src/Paints/Paint.js
+++ src/Paints/Paint.js
@@ -1,15 +1,14 @@
-import React, { useEffect, useState,useContext } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import data from "../Data/Data";
 import Product from "../Product/Product";
-import { UserContext } from './../UserContext/UserContext';
-
+import { UserContext } from "./../UserContext/UserContext";
 
 const Paint = (props) => {
   // const filtereddata = data.slice(1)
   const cartProduct = useContext(UserContext);
   const [allProducts, setAllProducts] = useState([]);
   const [images, setImages] = useState([]);
-  const[productFilter,setproductFilter]=useState([])
+  const [productFilter, setproductFilter] = useState([]);
 
   useEffect(() => {
     const paint = fetch(
@@ -21,41 +20,57 @@ const Paint = (props) => {
       })
       .then((paint) => {
         console.log(paint);
-        setAllProducts(paint.products);
+
+        //sort the result
+        const sortedProducts = paint.products.sort((a, b) => {
+          return a.price - b.price;
+        });
+
+        setAllProducts(sortedProducts);
         setImages(paint.files);
       });
   }, []);
- const handlelow=()=>{
-   console.log("low to high")
- }
-  const handleHigh = () => {
-    console.log("high to low ");
+
+  const handlelow = () => {
+    const sortedProducts = allProducts.sort((a, b) => {
+      return a.price - b.price;
+    });
+    setAllProducts([...sortedProducts]);
   };
+
+  const handleHigh = () => {
+    const sortedProducts = allProducts.sort((a, b) => {
+      return b.price - a.price;
+    });
+    setAllProducts([...sortedProducts]);
+  };
+
   return (
     <>
       <div className="row col-3" style={{ marginTop: 100 }}>
         <div>
-          <button onClick={handlelow} style={{backgroundColor:"aquamarine",marginRight: 20, marginLeft:37,padding:5}}>Low to High</button>
-          <button onClick={handleHigh} style={{backgroundColor:"aquamarine",padding:5}}>High to Low</button>
+          <button
+            onClick={handlelow}
+            style={{ backgroundColor: "aquamarine", marginRight: 20, marginLeft: 37, padding: 5 }}
+          >
+            Low to High
+          </button>
+          <button onClick={handleHigh} style={{ backgroundColor: "aquamarine", padding: 5 }}>
+            High to Low
+          </button>
         </div>
-        {allProducts
-          .sort((a, b) => {
-            return a.price - b.price;
-          })
-          .map((x, index) => {
-            return (
-              <Product
-                cartProduct
-                sname={x.name}
-                imgsrc={
-                  (images.find((item) => item.id === x.can_img_id) || {})
-                    .download_url
-                }
-                Price={x.price}
-                links={x.slug}
-              />
-            );
-          })}
+        {allProducts.map((x, index) => {
+          return (
+            <Product
+              key={index}
+              cartProduct
+              sname={x.name}
+              imgsrc={(images.find((item) => item.id === x.can_img_id) || {}).download_url}
+              Price={x.price}
+              links={x.slug}
+            />
+          );
+        })}
       </div>
     </>
   );
